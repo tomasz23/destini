c3a7861c0eb7e40507bde3ecc87e319012dff348
Tom Kieronski <tomasz@gemom.com.au>
2023-01-10 18:38:48 +0800

    simple flutter choose your own adventure - demo

---
+++ android/app/src/main/kotlin/au/com/genom/destini/destini/MainActivity.kt
@@ -0,0 +1,6 @@
+package au.com.genom.destini.destini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  int nextChoice1;
+  String choice2;
+  int nextChoice2;
+
+  Story(this.storyTitle, this.choice1, this.nextChoice1, this.choice2,
+      this.nextChoice2);
+}
